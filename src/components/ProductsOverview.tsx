a0ebc411c059a52a909794474c7fad14cf34f658
Sara Mali <mali.sara7@gmail.com>
2023-10-24 20:38:37 +0200

    products overview svg

--- src/components/ProductsOverview.tsx
+++ src/components/ProductsOverview.tsx
@@ -15,36 +15,53 @@ const categories = [
   { title: "Čebelje družine", img: Bees, url: "/cebelje-druzne" },
 ];
 
+//TODO change title color &|| style
 function ProductsOverview(): JSX.Element {
   return (
-    <>
-      <Title title="Ponudba" />
-      <div
-        className="flex flex-wrap justify-center gap-8 mt-10 w-[80%]
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#fef9c3"
+          fill-opacity="1"
+          d="M0,224L1440,64L1440,320L0,320Z"
+        ></path>
+      </svg>
+      <div className="bg-yellow-100">
+        <Title title="Ponudba" />
+        <div
+          className="flex flex-wrap justify-center gap-8 mt-10 w-[80%]
      md:w-[60%] mx-auto flex-col md:flex-row"
-      >
-        {categories.map((category) => {
-          return (
-            <div className="text-center flex flex-col items-center">
-              <NavLink to={category.url}>
-                <img
-                  src={category.img}
-                  alt=""
-                  className="h-[80vw] sm:h-[20rem] w-[20rem] 
+        >
+          {categories.map((category) => {
+            return (
+              <div className="text-center flex flex-col items-center">
+                <NavLink to={category.url}>
+                  <img
+                    src={category.img}
+                    alt=""
+                    className="h-[80vw] sm:h-[20rem] w-[20rem] 
                 object-cover md:mt-6 hover:opacity-80"
-                />
-                <div
-                  className="border border-yellow-100 p-2 mt-3
+                  />
+                  <div
+                    className="border border-yellow-100 p-2 mt-3
                bg-yellow-500 text-white text-lg hover:opacity-80"
-                >
-                  {category.title}
-                </div>
-              </NavLink>
-            </div>
-          );
-        })}
+                  >
+                    {category.title}
+                  </div>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#fef9c3"
+          fill-opacity="1"
+          d="M0,192L1440,128L1440,0L0,0Z"
+        ></path>
+      </svg>
+    </div>
   );
 }
 
