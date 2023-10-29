a5bb93a3b42c18b0521a43f1f1bf9e84ee1387ac
Sara Mali <mali.sara7@gmail.com>
2023-10-29 18:30:34 +0100

    products overview style change

--- src/components/homepage/ProductsOverview.tsx
+++ src/components/homepage/ProductsOverview.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { products_urls } from "../../utils/urls";
 import Title from "../../components/Title";
+import Button from "../Button";
 
 //TODO change title color &|| style
 function ProductsOverview(): JSX.Element {
@@ -8,16 +9,16 @@ function ProductsOverview(): JSX.Element {
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
-          fill="#fef9c3"
+          fill="#e8d1eb"
           fillOpacity="1"
           d="M0,224L1440,64L1440,320L0,320Z"
         ></path>
       </svg>
-      <div className="bg-yellow-100">
+      <div className="bg-primaryPurple">
         <Title title="Ponudba" />
         <div
           className="flex flex-wrap justify-center gap-8 mt-10 w-[80%]
-     md:w-[60%] mx-auto flex-col md:flex-row"
+          md:w-[60%] mx-auto flex-col md:flex-row"
         >
           {products_urls.map((product_url) => {
             return (
@@ -30,15 +31,10 @@ function ProductsOverview(): JSX.Element {
                     src={product_url.img}
                     alt=""
                     className="h-[80vw] sm:h-[20rem] w-[20rem] 
-                object-cover md:mt-6 hover:opacity-80"
+                  object-cover md:mt-6 hover:opacity-80"
                   />
-                  <div
-                    className="border border-yellow-100 p-2 mt-3
-               bg-yellow-500 text-white text-lg hover:opacity-80"
-                  >
-                    {product_url.title}
-                  </div>
                 </NavLink>
+                <Button text={product_url.title} url={product_url.url} />
               </div>
             );
           })}
@@ -46,7 +42,7 @@ function ProductsOverview(): JSX.Element {
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
-          fill="#fef9c3"
+          fill="#e8d1eb"
           fillOpacity="1"
           d="M0,192L1440,128L1440,0L0,0Z"
         ></path>
