4867c3998604e661eebfc59f516b64386bb66828
Sara Mali <mali.sara7@gmail.com>
2023-11-02 19:20:40 +0100

    btn style change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,10 +4,10 @@ type Props = { text: string; url: string };
 
 function Button({ text, url }: Props): JSX.Element {
   return (
-    <NavLink to={url}>
+    <NavLink to={url} className="w-[100%]">
       <div
         className="border hover:border-white p-2 mt-3 text-white 
-        text-lg hover:bg-secondaryPurple bg-transparent"
+        text-lg bg-secondaryPurple hover:bg-opacity-90"
       >
         {text}
       </div>
