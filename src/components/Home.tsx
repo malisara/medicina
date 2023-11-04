6b2446d814ce3f92feab9190917ea4ca5c248d80
Sara Mali <mali.sara7@gmail.com>
2023-11-04 13:13:24 +0100

    homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,17 @@
-type Props = {};
+import Testemonials from "./homepage/Testemonials";
+import Hero from "./homepage/Hero";
+import Presentation from "./homepage/Presentation";
+import ProductsOverview from "./homepage/ProductsOverview";
 
-function Home({}: Props) {
-  return <>home </>;
+function Home(): JSX.Element {
+  return (
+    <>
+      <Hero />
+      <Presentation />
+      <ProductsOverview />
+      <Testemonials />
+    </>
+  );
 }
 
 export default Home;
