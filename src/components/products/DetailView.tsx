772fcf6494dea79e89c9a05e66fd5e92c7bff7de
Sara Mali <mali.sara7@gmail.com>
2023-11-25 16:16:45 +0100

    detail view refactoring

--- src/components/products/DetailView.tsx
+++ src/components/products/DetailView.tsx
@@ -2,16 +2,11 @@ import { useState } from "react";
 import useMediaQuery from "../../hooks/useMediaQuery";
 import { imageCover } from "../homepage/Hero";
 import SizesAndPrices from "./SizesAndPrices";
+import { ProductDataType } from "../../typeAliases";
 
-const sizePriceData = [
-  //temp
-  { index: 0, size: "500ml", price: 10 },
-  { index: 1, size: "1000ml", price: 15 },
-];
+type Props = { productData: ProductDataType };
 
-type Props = { title: string; text: JSX.Element[]; imageSrc: string };
-
-function DetailView({ title, text, imageSrc }: Props): JSX.Element {
+function DetailView({ productData }: Props): JSX.Element {
   const isDesktop = useMediaQuery("(min-width: 1060px)");
   const [price, setPrice] = useState<number>(0);
 
@@ -27,22 +22,29 @@ function DetailView({ title, text, imageSrc }: Props): JSX.Element {
               className="transform -translate-x-[-40%] 
             -translate-y-[-10%] h-[40rem]"
             >
-              <img src={imageSrc} className={imageCover} />
+              <img src={productData.imageSrc} className={imageCover} />
             </div>
           </div>
           <div
-            className="w-full text-end px-[10rem] 
-          flex flex-col items-end"
+            className="w-full text-end pr-[15rem] 
+            pl-[5rem] flex flex-col items-end"
           >
-            <p className="py-[4rem] text-6xl">{title}</p>
-            <p className="text-5xl opacity-80">{price}€</p>
-            <SizesAndPrices
-              handlePriceChange={setPrice}
-              sizePriceData={sizePriceData}
-            />
+            <p className="py-[4rem] text-6xl">{productData.title}</p>
+
+            {productData.prices ? (
+              <>
+                <p className="text-5xl opacity-80">{price}€</p>
+                <SizesAndPrices
+                  handleSizeChange={setPrice}
+                  sizesPricesData={productData.prices}
+                />
+              </>
+            ) : (
+              <p className="text-5xl opacity-80">{productData.price}€</p>
+            )}
 
             <div className="mt-16 w-[70%] text-justify">
-              {text.map((paragraph, i) => (
+              {productData.text.map((paragraph, i) => (
                 <div key={i} className="mb-6 leading-8">
                   {paragraph}
                 </div>
@@ -52,13 +54,32 @@ function DetailView({ title, text, imageSrc }: Props): JSX.Element {
         </div>
       ) : (
         <div>
-          <h2>{title}</h2>
-          <div className="w-[85%] mx-auto">
+          <div className="w-[85%] mx-auto mt-10">
             <div className="h-[25rem] sm:h-[25rem]">
-              <img src={imageSrc} className={imageCover} />
+              <img src={productData.imageSrc} className={imageCover} />
             </div>
-            <div className="mt-10 text-justify">
-              {text.map((paragraph, i) => (
+            <p className="py-[3rem] text-4xl text-center">
+              {productData.title}
+            </p>
+
+            {productData.prices ? (
+              <>
+                <p className="text-3xl text-center opacity-80">{price}€</p>
+                <div className="flex justify-center mb-10">
+                  <SizesAndPrices
+                    handleSizeChange={setPrice}
+                    sizesPricesData={productData.prices}
+                  />
+                </div>
+              </>
+            ) : (
+              <p className="text-5xl opacity-80 mx-auto">
+                {productData.price}€
+              </p>
+            )}
+
+            <div className="text-justify pb-10">
+              {productData.text.map((paragraph, i) => (
                 <div key={i} className="mb-6 leading-8">
                   {paragraph}
                 </div>
