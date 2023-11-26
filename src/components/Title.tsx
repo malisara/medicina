16aaeae201c4d699fa458b42b39a123f10e4af36
Sara Mali <mali.sara7@gmail.com>
2023-11-26 18:08:44 +0100

    title style change

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,11 @@
 type Props = { title: string };
 
+//TODO mobile title height
 function Title({ title }: Props): JSX.Element {
   return (
     <div
-      className="text-5xl text-yellow-500 
-    mb-[5rem] text-center"
+      className="text-5xl text-titleColor 
+    mb-[5rem] text-center font-semibold"
     >
       {title}
     </div>
