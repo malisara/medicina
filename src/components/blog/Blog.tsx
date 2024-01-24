061ed438a9ad7098219936005b32a5e3899b6fd2
Sara Mali <mali.sara7@gmail.com>
2024-01-24 21:59:23 +0100

    blog component update

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import Announcements from "./Announcements";
 import Recipes from "./Recipes";
-//TODO add other blog components or rename component to 'Recipes'
 
-function Blog(): JSX.Element {
-  return <Recipes />;
+const baseBtnStyle =
+  "border border-secondaryPurple rounded-sm py-2 px-4 uppercase";
+const chosenBtnStyle = "bg-secondaryPurple text-white hover:opacity-80";
+const otherBtnStyle = "hover:bg-secondaryPurple text-gray-500 hover:text-white";
+
+function Blog() {
+  const [recipesChosen, setRecipesChosen] = useState(true);
+  return (
+    // buttons
+    <div>
+      <div className="flex justify-center gap-5 mt-9">
+        <button
+          className={`${baseBtnStyle} ${
+            recipesChosen ? chosenBtnStyle : otherBtnStyle
+          }`}
+          onClick={() => setRecipesChosen(true)}
+        >
+          recepti
+        </button>
+        <button
+          className={`${baseBtnStyle} ${
+            !recipesChosen ? chosenBtnStyle : otherBtnStyle
+          }`}
+          onClick={() => setRecipesChosen(false)}
+        >
+          obvestila
+        </button>
+      </div>
+
+      {/* Content */}
+      {recipesChosen ? <Recipes /> : <Announcements />}
+    </div>
+  );
 }
 
 export default Blog;
